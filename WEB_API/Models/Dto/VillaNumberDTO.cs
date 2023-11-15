385da365fb3495cc4b76138cf44c1fdd643ad8b7
mahfuz khan <mahfuzpust@gmail.com>
2023-11-15 22:55:29 +0600

    Getting Villa and retriving Villa Number

--- WEB_API/Models/Dto/VillaNumberDTO.cs
+++ WEB_API/Models/Dto/VillaNumberDTO.cs
@@ -9,5 +9,6 @@ namespace WEB_API.Models.Dto
         [Required]
         public int VillaID { get; set; }
         public string SpecialDetails { get; set; }
+        public VillaDTO Villa { get; set; }
     }
 }
