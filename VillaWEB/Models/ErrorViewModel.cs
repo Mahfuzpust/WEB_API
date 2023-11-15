8add95580978758b3ceb849dbc79eb8ea5625fad
mahfuz khan <mahfuzpust@gmail.com>
2023-11-15 06:47:08 +0600

    Setup MVC web project and setup port

---
+++ VillaWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VillaWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
