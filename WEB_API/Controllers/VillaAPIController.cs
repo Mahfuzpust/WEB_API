6890c6ea457be07c87b135d5e8ca5aae7f5e6b6f
mahfuz khan <mahfuzpust@gmail.com>
2023-11-29 11:24:03 +0600

    Special validation and base services

--- WEB_API/Controllers/VillaAPIController.cs
+++ WEB_API/Controllers/VillaAPIController.cs
@@ -89,7 +89,7 @@ namespace WEB_API.Controllers
             {
                 if (await _dbVilla.GetAsync(u => u.Name.ToLower() == createDTO.Name.ToLower()) != null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa already Exists!");
+                    ModelState.AddModelError("ErrorMessages", "Villa already Exists!");
                     return BadRequest(ModelState);
                 }
                 if (createDTO == null)
--- WEB_API/Controllers/VillaNumberAPIController.cs
+++ WEB_API/Controllers/VillaNumberAPIController.cs
@@ -92,13 +92,13 @@ namespace WEB_API.Controllers
 
                 if (await _dbVillaNumber.GetAsync(u => u.VillaNo == createDTO.VillaNo) != null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa Number already Exists!");
+                    ModelState.AddModelError("ErrorMessages", "Villa Number already Exists!");
                     return BadRequest(ModelState);
                 }
 
                 if(await _dbVilla.GetAsync(u => u.Id == createDTO.VillaID) == null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa ID is Invalid!");
+                    ModelState.AddModelError("ErrorMessages", "Villa ID is Invalid!");
                     return BadRequest(ModelState);
                 }
 
@@ -169,7 +169,7 @@ namespace WEB_API.Controllers
 
                 if (await _dbVilla.GetAsync(u => u.Id == updateDTO.VillaID) == null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa ID is Invalid!");
+                    ModelState.AddModelError("ErrorMessages", "Villa ID is Invalid!");
                     return BadRequest(ModelState);
                 }
 
